71f38cfe8c4c26103dbed5df7af1ccd7cf837316
unknown <hamzariaz1994@gmail.com>
2020-08-31 11:25:05 +0500

    commit check by hamza

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -10,23 +10,4 @@ export class Globals {
   userId: string = '';
   userName: string ="";
 
-  // for lease and all 
-  selectedJournal = "";
-  leaseContractNo = "";
-  classAsset = "";
-  commencementDate;
-  paymentsAt = "";
-  annualDiscountRate = "";
-  leaseTerm = "";
-  expectedPeriod = "";
-  leasePayment = "";
-  paymentIntervals = "";
-  initialDirectCost = "";
-  guaranteedResidualValue = "";
-  usefulLifeOfTheAsset = "";
-  escalation = "";
-  escalationAfterEvery = "";
-  presentValue = 0;
-
-
 }
