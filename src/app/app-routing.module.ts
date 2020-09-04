987a6cb6004e706dd2ea68414bacf95f22a3b470
unknown <hamzariaz1994@gmail.com>
2020-09-04 17:17:10 +0500

    sisgnup  module added and minor changes related to css done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PostsComponent } from './posts/posts.component';
 import { AddPostsComponent } from './add-posts/add-posts.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'post', component: PostsComponent },
   { path: '', redirectTo: '/post', pathMatch: 'full' },
   { path: 'addPost', component: AddPostsComponent },
+  { path: 'signup', component: SignupComponent },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     PostsComponent,
     AddPostsComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
